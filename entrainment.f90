0d418905fdaf02d80332fbf555e6b55c86f6df7b
Ian Glenn <ian.glenn@utah.edu>
2014-06-03 17:39:18 -0600

    Update entrainment.f90

--- entrainment.f90
+++ entrainment.f90
@@ -1,12 +1,25 @@
-subroutine entrainment ( jt_in, kt_in )
+SUBROUTINE entrainment ()
 
+use global_vars
+! only cldarea(jt, kt, 2) ? 
 implicit none
 
 ! the 4 main arrays
-integer, parameter :: jt = jt_in
-integer, parameter :: kt = kt_in
 
 real interp_d ( jt+1, kt+1 )
 real lr_edges ( jt+1, kt )
 real tb_edges ( jt, kt+1 )
-! real cldarea ( jt, kt, 2 )
+
+! make the interp_d grid
+! must choose what the real number d is based off of
+! such that d >= 0 is "convective" and d < 0 not
+! for example, d cannot be set equal to qc because qc cannot have values less than zero
+! after Dawe and Austin (2011) we define d as q_diff which is the excess of total water
+! over the saturation specific humidity
+
+! first find qsat(T,p)
+
+do J = 1, jt+1
+  do K = 1, kt+1
+    interp_d(J, K) = 
+
