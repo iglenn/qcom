eb0f0702880daea2e46cb51153b2f89ed43e0bfa
Ian Glenn <ian.glenn@utah.edu>
2014-06-04 19:32:50 -0600

    Update qcom.f90

--- qcom.f90
+++ qcom.f90
@@ -646,8 +646,8 @@ real interp_d ( jt+1, kt+1 ) ! the convective condition
 real lr_edges ( jt+1, kt ) ! left - right grid edges
 real tb_edges ( jt, kt+1 ) ! top - bottom grid edges
 
-integer, dimension(4,1) :: onec, threec, two_adj
-integer, dimension(2,1) :: two_opp
+integer, dimension(1:4, 1) :: onec, threec, two_adj
+integer, dimension(1:2, 1) :: two_opp
 integer mcode
 logical a, b, c, d
 
@@ -709,28 +709,28 @@ do J = 1, jt
       if (mcode == 1) then ! it's d
         lr_edges(J,K) = - interp_d(J, K) * dz / ( interp_d(J, K+1) - interp_d(J, K)  )
         tb_edges(J,K) = - interp_d(J, K) * dy / ( interp_d(J+1, K) - interp_d(J, K)  )
-        cldarea(J,K) = 0.5 * lr_edges(J,K) * tb_edges(J,K) 
+        cldarea(J,K,2) = 0.5 * lr_edges(J,K) * tb_edges(J,K) 
         udotw(J,K) = lr_edges(J,K) * v(J+1,K+1) + tb_edges(J,K) * w(J+1, K+1)
         CYCLE
       end if
       if (mcode == 2) then ! it's c
         lr_edges(J+1,K) = - interp_d(J+1, K) * dz / ( interp_d(J+1, K+1) - interp_d(J+1, K)  )
         tb_edges(J,K) = - interp_d(J+1, K) * dy / ( interp_d(J, K) - interp_d(J+1, K)  )
-        cldarea(J,K) = 0.5 * lr_edges(J+1,K) * tb_edges(J,K)
+        cldarea(J,K,2) = 0.5 * lr_edges(J+1,K) * tb_edges(J,K)
         udotw(J,K) = lr_edges(J+1,K) * v(J+2,K+1) + tb_edges(J,K) * w(J+1, K+1)
         CYCLE
       end if
       if (mcode == 4) then ! it's b
         lr_edges(J+1,K) = - interp_d(J+1, K+1) * dz / ( interp_d(J+1, K) - interp_d(J+1, K+1)  )
         tb_edges(J,K+1) = - interp_d(J+1, K+1) * dy / ( interp_d(J, K+1) - interp_d(J+1, K+1)  )
-        cldarea(J,K) = 0.5 * lr_edges(J+1,K) * tb_edges(J,K+1)
+        cldarea(J,K,2) = 0.5 * lr_edges(J+1,K) * tb_edges(J,K+1)
         udotw(J,K) = lr_edges(J+1,K) * v(J+2,K+1) + tb_edges(J,K+1) * w(J+1, K+2)
         CYCLE
       end if
       if (mcode == 8) then ! it's a
         lr_edges(J,K) = - interp_d(J, K+1) * dz / ( interp_d(J, K) - interp_d(J, K+1)  )
         tb_edges(J,K+1) = - interp_d(J, K+1) * dy / ( interp_d(J+1, K+1) - interp_d(J, K+1)  )
-        cldarea(J,K) = 0.5 * lr_edges(J,K) * tb_edges(J,K+1)
+        cldarea(J,K,2) = 0.5 * lr_edges(J,K) * tb_edges(J,K+1)
         udotw(J,K) = lr_edges(J,K) * v(J+1,K+1) + tb_edges(J,K+1) * w(J+1, K+2)
         CYCLE
       end if
@@ -742,28 +742,28 @@ do J = 1, jt
       if (mcode == 14) then ! it's d
         lr_edges(J,K) = interp_d(J, K) * dz / ( interp_d(J, K+1) - interp_d(J, K)  )
         tb_edges(J,K) = interp_d(J, K) * dy / ( interp_d(J+1, K) - interp_d(J, K)  )
-        cldarea(J,K) = ( dy * dz ) - 0.5 * lr_edges(J,K) * tb_edges(J,K) 
+        cldarea(J,K,2) = ( dy * dz ) - 0.5 * lr_edges(J,K) * tb_edges(J,K) 
         udotw(J,K) = ( dz - lr_edges(J,K) ) * v(J+1,K+1) + ( dy - tb_edges(J,K) ) * w(J+1, K+1) + dz * v(J+2,K+1) + dy * w(J+1,K+2)
         CYCLE
       end if
       if (mcode == 13) then ! it's c
         lr_edges(J+1,K) = interp_d(J+1, K) * dz / ( interp_d(J+1, K+1) - interp_d(J+1, K)  )
         tb_edges(J,K) = interp_d(J+1, K) * dy / ( interp_d(J, K) - interp_d(J+1, K)  )
-        cldarea(J,K) = ( dy * dz ) -  0.5 * lr_edges(J+1,K) * tb_edges(J,K)
+        cldarea(J,K,2) = ( dy * dz ) -  0.5 * lr_edges(J+1,K) * tb_edges(J,K)
         udotw(J,K) = ( dz - lr_edges(J+1,K) ) * v(J+2,K+1) + ( dy - tb_edges(J,K) ) * w(J+1, K+1) + dz * v(J+1,K+1) + dy * w(J+1,K+2)
         CYCLE
       end if
       if (mcode == 11) then ! it's b
         lr_edges(J+1,K) = interp_d(J+1, K+1) * dz / ( interp_d(J+1, K) - interp_d(J+1, K+1)  )
         tb_edges(J,K+1) = interp_d(J+1, K+1) * dy / ( interp_d(J, K+1) - interp_d(J+1, K+1)  )
-        cldarea(J,K) = ( dy * dz ) - 0.5 * lr_edges(J+1,K) * tb_edges(J,K+1) 
+        cldarea(J,K,2) = ( dy * dz ) - 0.5 * lr_edges(J+1,K) * tb_edges(J,K+1) 
         udotw(J,K) = ( dz - lr_edges(J+1,K) ) * v(J+2,K+1) + ( dy - tb_edges(J,K+1) ) * w(J+1, K+2) + dz * v(J+1,K+1) + dy * w(J+1,K+1)
         CYCLE
       end if
       if (mcode == 7) then ! it's a
         lr_edges(J,K) = interp_d(J, K+1) * dz / ( interp_d(J, K) - interp_d(J, K+1)  )
         tb_edges(J,K+1) = interp_d(J, K+1) * dy / ( interp_d(J+1, K+1) - interp_d(J, K+1)  )
-        cldarea(J,K) =  ( dy * dz ) - 0.5 * lr_edges(J,K) * tb_edges(J,K+1)
+        cldarea(J,K,2) =  ( dy * dz ) - 0.5 * lr_edges(J,K) * tb_edges(J,K+1)
         udotw(J,K) = ( dz - lr_edges(J,K) ) * v(J+1,K+1) + ( dy - tb_edges(J,K+1) ) * w(J+1, K+2) + dz * v(J+2,K+1) + dy * w(J+1,K+1)
         CYCLE
       end if
@@ -776,7 +776,7 @@ do J = 1, jt
         lr_edges(J,K) = - interp_d(J, K) * dz / ( interp_d(J,K) - interp_d(J, K+1) )
         ! from c to b
         lr_edges(J+1,K) =  - interp_d(J+1, K) * dz / ( interp_d(J+1,K+1) - interp_d(J+1, K) )
-        cldarea(J,K) = 0.5 * dy * lr_edges(J,K) * lr_edges(J+1,K)
+        cldarea(J,K,2) = 0.5 * dy * lr_edges(J,K) * lr_edges(J+1,K)
         udotw(J,K) = lr_edges(J,K) * v(J+1,K+1) + lr_edges(J+1,K) * v(J+2,K+1) + dy * w(J+1,K+1)
         CYCLE
       end if
@@ -785,7 +785,7 @@ do J = 1, jt
         lr_edges(J,K) = dz - interp_d(J, K) * dz / ( interp_d(J,K) - interp_d(J, K+1) )
         ! from c to b
         lr_edges(J+1,K) = dz - interp_d(J+1, K) * dz / ( interp_d(J+1,K+1) - interp_d(J+1, K) )
-        cldarea(J,K) = 0.5 * dy * lr_edges(J,K) * lr_edges(J+1,K)
+        cldarea(J,K,2) = 0.5 * dy * lr_edges(J,K) * lr_edges(J+1,K)
         udotw(J,K) = lr_edges(J,K) * v(J+1,K+1) + lr_edges(J+1,K) * v(J+2,K+1) + dy * w(J+1,K+2)
         CYCLE
       end if
@@ -794,7 +794,7 @@ do J = 1, jt
         tb_edges(J,K) = - interp_d(J, K) * dy / ( interp_d(J+1,K) - interp_d(J, K) )
         ! from a to b
         tb_edges(J,K+1) =  - interp_d(J, K+1) * dy / ( interp_d(J+1,K+1) - interp_d(J, K+1) )
-        cldarea(J,K) = 0.5 * dz * tb_edges(J,K) * tb_edges(J,K+1)
+        cldarea(J,K,2) = 0.5 * dz * tb_edges(J,K) * tb_edges(J,K+1)
         udotw(J,K) = tb_edges(J,K) * w(J+1,K+1) + tb_edges(J,K+1) * w(J+1,K+2) + dz * v(J+1,K+1)
         CYCLE
       end if
@@ -803,7 +803,7 @@ do J = 1, jt
         tb_edges(J,K) = dy - interp_d(J, K) * dy / ( interp_d(J+1,K) - interp_d(J, K) )
         ! from a to b
         tb_edges(J,K+1) = dy - interp_d(J, K+1) * dy / ( interp_d(J+1,K+1) - interp_d(J, K+1) )
-        cldarea(J,K) = 0.5 * dz * tb_edges(J,K) * tb_edges(J,K+1)
+        cldarea(J,K,2) = 0.5 * dz * tb_edges(J,K) * tb_edges(J,K+1)
         udotw(J,K) = tb_edges(J,K) * w(J+1,K+1) + tb_edges(J,K+1) * w(J+1,K+2) + dz * v(J+2,K+1)
         CYCLE
       end if
@@ -820,7 +820,7 @@ do J = 1, jt
         lr_edges(J,K) = - interp_d(J, K) * dz / ( interp_d(J, K+1) - interp_d(J, K)  )
         ! d to c
         tb_edges(J,K) = - interp_d(J, K) * dy / ( interp_d(J+1, K) - interp_d(J, K)  )
-        cldarea(J,K) = 0.5 * ( lr_edges(J+1,K) * tb_edges(J,K+1) + lr_edges(J,K) * tb_edges(J,K) )
+        cldarea(J,K,2) = 0.5 * ( lr_edges(J+1,K) * tb_edges(J,K+1) + lr_edges(J,K) * tb_edges(J,K) )
         udotw(J,K) = lr_edges(J,K) * v(J+1,K+1) + lr_edges(J+1,K) * v(J+2,K+1) + tb_edges(J,K) * w(J+1,K+1) + tb_edges(J,K+1) * w(J+1,K+2)
         CYCLE
       end if
@@ -833,7 +833,7 @@ do J = 1, jt
         lr_edges(J,K) = dz - interp_d(J, K) * dz / ( interp_d(J, K+1) - interp_d(J, K)  )
         ! d to c
         tb_edges(J,K) = dy - interp_d(J, K) * dy / ( interp_d(J+1, K) - interp_d(J, K)  )
-        cldarea(J,K) = 0.5 * ( lr_edges(J+1,K) * tb_edges(J,K+1) + lr_edges(J,K) * tb_edges(J,K) )
+        cldarea(J,K,2) = 0.5 * ( lr_edges(J+1,K) * tb_edges(J,K+1) + lr_edges(J,K) * tb_edges(J,K) )
         udotw(J,K) = lr_edges(J,K) * v(J+1,K+1) + lr_edges(J+1,K) * v(J+2,K+1) + tb_edges(J,K) * w(J+1,K+1) + tb_edges(J,K+1) * w(J+1,K+2)
         CYCLE
       end if
